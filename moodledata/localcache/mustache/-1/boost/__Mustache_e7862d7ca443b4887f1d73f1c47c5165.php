004307f5cbc64cd509bcb008beb692efa2044e8e
haritha-j <harithajayasinghe@gmail.com>
2018-05-12 08:22:23 +0530

    First commit

---
+++ moodledata/localcache/mustache/-1/boost/__Mustache_e7862d7ca443b4887f1d73f1c47c5165.php
@@ -0,0 +1,131 @@
+<?php
+
+class __Mustache_e7862d7ca443b4887f1d73f1c47c5165 extends Mustache_Template
+{
+    private $lambdaHelper;
+
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $this->lambdaHelper = new Mustache_LambdaHelper($this->mustache, $context);
+        $buffer = '';
+
+        if ($parent = $this->mustache->loadPartial('core_form/element-template')) {
+            $context->pushBlockContext(array(
+                'element' => array($this, 'blockFd66614842795477b95d59dd5677e8cb'),
+            ));
+            $buffer .= $parent->renderInternal($context, $indent);
+            $context->popBlockContext();
+        }
+
+        return $buffer;
+    }
+
+    private function section5968b0313ca3957f90dc584bb9649f31(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = 'form-control-danger';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= 'form-control-danger';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    private function section4016666d4bbffd7770ad7606054c5611(Mustache_Context $context, $indent, $value)
+    {
+        $buffer = '';
+    
+        if (!is_string($value) && is_callable($value)) {
+            $source = '
+                    autofocus aria-describedby="id_error_{{element.name}}"
+                ';
+            $result = call_user_func($value, $source, $this->lambdaHelper);
+            if (strpos($result, '{{') === false) {
+                $buffer .= $result;
+            } else {
+                $buffer .= $this->mustache
+                    ->loadLambda((string) $result)
+                    ->renderInternal($context);
+            }
+        } elseif (!empty($value)) {
+            $values = $this->isIterable($value) ? $value : array($value);
+            foreach ($values as $value) {
+                $context->push($value);
+                
+                $buffer .= $indent . '                    autofocus aria-describedby="id_error_';
+                $value = $this->resolveValue($context->findDot('element.name'), $context);
+                $buffer .= call_user_func($this->mustache->getEscape(), $value);
+                $buffer .= '"
+';
+                $buffer .= $indent . '                ';
+                $context->pop();
+            }
+        }
+    
+        return $buffer;
+    }
+
+    public function blockFd66614842795477b95d59dd5677e8cb($context)
+    {
+        $indent = $buffer = '';
+        // 'element.frozen' inverted section
+        $value = $context->findDot('element.frozen');
+        if (empty($value)) {
+            
+            $buffer .= $indent . '        <input type="password"
+';
+            $buffer .= $indent . '                class="form-control ';
+            // 'error' section
+            $value = $context->find('error');
+            $buffer .= $this->section5968b0313ca3957f90dc584bb9649f31($context, $indent, $value);
+            $buffer .= '"
+';
+            $buffer .= $indent . '                name="';
+            $value = $this->resolveValue($context->findDot('element.name'), $context);
+            $buffer .= call_user_func($this->mustache->getEscape(), $value);
+            $buffer .= '"
+';
+            $buffer .= $indent . '                id="';
+            $value = $this->resolveValue($context->findDot('element.id'), $context);
+            $buffer .= call_user_func($this->mustache->getEscape(), $value);
+            $buffer .= '"
+';
+            $buffer .= $indent . '                value="';
+            $value = $this->resolveValue($context->findDot('element.value'), $context);
+            $buffer .= call_user_func($this->mustache->getEscape(), $value);
+            $buffer .= '"
+';
+            $buffer .= $indent . '                size="';
+            $value = $this->resolveValue($context->findDot('element.size'), $context);
+            $buffer .= call_user_func($this->mustache->getEscape(), $value);
+            $buffer .= '"
+';
+            // 'error' section
+            $value = $context->find('error');
+            $buffer .= $this->section4016666d4bbffd7770ad7606054c5611($context, $indent, $value);
+            $buffer .= ' ';
+            $value = $this->resolveValue($context->find('attributes'), $context);
+            $buffer .= $value;
+            $buffer .= '>
+';
+        }
+    
+        return $buffer;
+    }
+}
